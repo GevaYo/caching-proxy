e236108bb08a9a09f6517eac1bc23a63ec7a2199
GevaYo <gevayo@mta.ac.il>
2024-09-07 13:56:07 +0300

    added 5000ms timeout for a request

--- httpClient.js
+++ httpClient.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 class HttpClient {
   static async fetchFromOrigin(url) {
     try {
-      const response = await axios.get(url);
+      const response = await axios.get(url, { timeout: 5000 }); // Added 5 second timeout
       return {
         status: response.status,
         headers: response.headers,
